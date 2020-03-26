401c301202ec1cb5baac208a770b163bb2004b65
Kunal <kunaldeshmukh2503@gmail.com>
2020-03-26 05:58:12 -0700

    Use case 2

--- TTT.sh
+++ TTT.sh
@@ -5,4 +5,15 @@ reset () {
 	arr=(- - - - - - - - -)
 }
 
+firstPlay () {
+	read -p"Enter 1 or 0 for a toss " toss
+	if [ $((RANDOM%2)) -eq $toss ]
+	then
+		read -p"Enter X or O" player
+	else
+		[ $((RANDOM%2)) -eq 1 ] && comp=X  || comp=O
+	fi
+}
+
 reset
+firstPlay
