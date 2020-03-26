70667c25891eaeda7ad9ebbd898527d0da0eb53a
Kunal <kunaldeshmukh2503@gmail.com>
2020-03-26 10:07:19 -0700

    default

--- TTT.sh
+++ TTT.sh
@@ -10,22 +10,44 @@ firstPlay () {
 	if [ $((RANDOM%2)) -eq $toss ]
 	then
 		read -p"Enter X or O" player
-		[[ "$player" -eq "X" ]] && comp=O || comp=X
+		if [ "$player" = "X" ]
+		then
+			comp=O
+			flag=player 
+		else 
+			comp=X
+			echo $comp	
+			flag=comp
+		fi
 	else
 		[ $((RANDOM%2)) -eq 1 ] && comp=X  || comp=O
-		[[ "$comp" -eq "X" ]] && player=O || player=X
+		if [ "$comp" = "X" ]
+		then
+			player=O 
+			flag=comp
+		else
+			player=X
+			flag=player
+		fi
 	fi
 }
 
 checkWinner () {
-	[ arr[$1] -eq arr[$2] -a arr[$2] -eq arr[$3]] && return arr[$1] || return break 
-	if [ $remMoves -eq 1 ]
+	index1=$1
+	index2=$2
+	index3=$3
+	if [ "${arr[$index1]}" != "-" ]
 	then
-	return break
+		[ "${arr[$index1]}" = "${arr[$index2]}" -a "${arr[$index2]}" = "${arr[$index3]}" ] && winner=${arr[$1]} || return 0
+		if [ $remMoves -eq 0 ]
+		then
+		winner=Tie
+		fi
 	fi
 }
 
 printBoard () {
+	echo
 	for i in {0..8}
 	do
 		[ $i -eq 2 -o $i -eq 5 ] && echo ${arr[$i]} || echo -n ${arr[$i]}
@@ -36,37 +58,59 @@ printBoard () {
 play () {
 	if [ "$flag" =  "player" ]
 	then
-	read -p"Enter position" pos
-	if [ "${arr[$pos]}" = "-" ] 
-	then
-	 	arr[$pos]=$player 
-		remMoves=$((remMoves-1))	
+		read -p"Enter position" pos
+		if [ "${arr[$((pos-1))]}" = "-" ] 
+		then
+	 		arr[$((pos-1))]=$player 
+			remMoves=$((remMoves-1))	
+		else
+	 		echo "Please enter any another locaation"
+			printBoard
+			play
+		fi
+		flag=comp
 	else
-	 	echo "Please enter any another locaation"
-		printBoard
-		play
-	fi
-	flag=comp
-	else
-	pos=$((RANDOM%remMoves))
-	if [ "${arr[$pos]}" = "-" ] 
-	then 
-		arr[$pos]=$comp 
-		remMoves=$((remMoves-1))
-	else
-		play
-		remMoves=$((remMoves-1))
-	fi 
-	flag=player
+		pos=$((RANDOM%9))
+		if [ "${arr[$pos]}" = "-" ] 
+		then 
+			arr[$pos]=$comp 
+			remMoves=$((remMoves-1))
+		else
+			play
+			
+		fi 
+		flag=player
 	fi
 	printBoard
 }
-flag=player
+flag=0
 remMoves=9
 reset
 firstPlay
 printBoard
+winner=no
+
 while true
 do
+	if [ $remMoves -lt 5 ]
+	then
+		checkWinner 0 1 2
+		checkWinner 3 4 5
+		checkWinner 6 7 8
+		checkWinner 0 4 8
+		checkWinner 2 4 6
+		checkWinner 0 3 6
+		checkWinner 1 4 7
+		checkWinner 2 5 8
+		if [ "$winner" = "Tie" ]
+		then
+			echo Tie
+			break
+		elif [ "$winner" = "X" -o "$?" = "O" ]
+		then
+			echo $winner "Won"
+			break
+		fi
+	fi
 	play
 done
