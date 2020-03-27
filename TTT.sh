85d3ac47e9d8f5adacd9acdb8d510b50c6e30287
Kunal <kunaldeshmukh2503@gmail.com>
2020-03-27 00:52:23 -0700

    Use case 7

--- TTT.sh
+++ TTT.sh
@@ -38,8 +38,40 @@ checkWinner () {
 	index3=$3
 	if [ "${arr[$index1]}" != "-" ]
 	then
-		[ "${arr[$index1]}" = "${arr[$index2]}" -a "${arr[$index2]}" = "${arr[$index3]}" ] && winner=${arr[$1]} || return 0
+		[ "${arr[$index1]}" = "${arr[$index2]}" -a "${arr[$index2]}" = "${arr[$index3]}" ] && winner=${arr[$index1]} || return 0
 	fi
+}
+
+checkTwo () {
+	if [ "$flag" = "player" ]
+	then
+		return 
+	fi
+	index1=$1
+	index2=$2
+	index3=$3
+	if [ "${arr[$index1]}" = "${arr[$index2]}" -a "${arr[$index1]}" != "-" -a "${arr[$index3]}" = "-" ] 
+	then
+		arr[$index3]=$comp
+	 	flag=player
+		remMoves=$((remMoves-1))
+		printBoard
+	fi
+	if [ "${arr[$index1]}" = "${arr[$index3]}" -a "${arr[$index1]}" != "-" -a "${arr[$index2]}" = "-" ]
+	then
+	 	arr[$index2]=$comp
+		remMoves=$((remMoves-1))
+	 	flag=player
+		printBoard
+	fi
+	if [ "${arr[$index3]}" = "${arr[$index2]}" -a "${arr[$index3]}" != "-" -a "${arr[$index1]}" = "-" ]
+	then
+		arr[$index1]=$comp
+		flag=player
+		printBoard
+		remMoves=$((remMoves-1))
+	fi
+
 }
 
 printBoard () {
@@ -51,33 +83,55 @@ printBoard () {
 	echo
 }
 
+randomPlay () {
+	pos=$((RANDOM%9))
+	if [ "${arr[$pos]}" = "-" ] 
+	then 
+		arr[$pos]=$comp 
+		remMoves=$((remMoves-1))
+	else
+		randomPlay
+	fi
+	flag=player
+	printBoard
+
+}
+
+winCheck () {
+	checkTwo 0 1 2
+	checkTwo 3 4 5
+	checkTwo 6 7 8
+		checkTwo 0 4 8
+		checkTwo 2 4 6
+		checkTwo 0 3 6
+		checkTwo 1 4 7
+		checkTwo 2 5 8
+		[ "$flag" = "comp" ] && randomPlay || :
+}
+
 play () {
 	if [ "$flag" =  "player" ]
 	then
 		read -p"Enter position" pos
-		if [ "${arr[$((pos-1))]}" = "-" ] 
+		if [ "${arr[$((pos-1))]}" = "-" ]
 		then
-	 		arr[$((pos-1))]=$player 
-			remMoves=$((remMoves-1))	
+	 		arr[$((pos-1))]=$player
+			remMoves=$((remMoves-1))
+			printBoard
 		else
 	 		echo "Please enter any another locaation"
 			printBoard
 			play
 		fi
 		flag=comp
+	elif [ $difficulty -eq 2 -a "$flag" = "comp" ]
+	then
+		winCheck
 	else
-		pos=$((RANDOM%9))
-		if [ "${arr[$pos]}" = "-" ] 
-		then 
-			arr[$pos]=$comp 
-			remMoves=$((remMoves-1))
-		else
-			play
-		fi
-		flag=player
+		randomPlay
 	fi
-	printBoard
 }
+read -p"Enter difficulty 1.Easy 2.Hard" difficulty 
 flag=0
 remMoves=9
 reset
@@ -87,11 +141,6 @@ winner=no
 
 while true
 do
-	if [ $remMoves -eq 0 ]
-	then
-		echo "Tie"
-		break
-	fi
 	if [ $remMoves -lt 5 ]
 	then
 		checkWinner 0 1 2
@@ -106,11 +155,16 @@ do
 		then
 			echo Tie
 			break
-		elif [ "$winner" = "X" -o "$?" = "O" ]
+		elif [ "$winner" = "X" -o "$winner" = "O" ]
 		then
 			echo $winner "Won"
 			break
 		fi
+	fi
+	if [ $remMoves -eq 0 ]
+	then
+		echo "Tie"
+		break
 	fi
 	play
 done
